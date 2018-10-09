3463ab9a159961136971c1f76c9d46d9033d5430
Ryan James Paed Apellanes <43224497+RyanJamesPaedApellanes@users.noreply.github.com>
2018-10-09 10:43:59 -0700

    Add files via upload

---
+++ PizzareaClient/Others/PizzaTableViewCell.swift
@@ -0,0 +1,21 @@
+﻿//
+//  PizzaTableViewCell.swift
+//  pizzarea
+//
+//  Created by Ryan Apellanes on 04/02/2018.
+//  Copyright © 2018 CreativityKills Co. All rights reserved.
+//
+
+import UIKit
+
+class PizzaTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var pizzaImageView: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var miscellaneousText: UILabel!
+    @IBOutlet weak var amount: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
